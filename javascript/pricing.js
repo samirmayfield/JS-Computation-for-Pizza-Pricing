35d105a321f0f8c8055dff86aa5d84f1798b6514
Samir Mayfield <samirmayfield@live.com>
2018-06-30 03:09:33 -0700

    Add JS computation for pizza pricing

---
+++ javascript/pricing.js
@@ -0,0 +1,169 @@
+function getReceipt() {
+	// This initializes our string so it can get passed from  
+	// function to function, growing line by line into a full receipt
+	var text1 = "<h3>You Ordered:</h3>";
+	var text2 = "<h3>blah</h3>"
+	var runningTotal = 0;
+	var sizeTotal = 0;
+	var sizeArray = document.getElementsByClassName("size");
+	for (var i = 0; i < sizeArray.length; i++) {
+		if (sizeArray[i].checked) {
+			var selectedSize = sizeArray[i].value;
+			text1 = text1+selectedSize+"<br>";
+		}
+	}
+	if (selectedSize === "Personal Pizza") {
+		sizeTotal = 6;
+	} else if (selectedSize === "Medium Pizza") {
+		sizeTotal = 10;
+	} else if (selectedSize === "Large Pizza") {
+		sizeTotal = 14;
+	} else if (selectedSize === "Extra Large Pizza") {
+		sizeTotal = 16;
+	}
+	runningTotal = sizeTotal;
+	console.log(selectedSize+" = $"+sizeTotal+".00");
+	console.log("size text1: "+text1);
+	console.log("subtotal: $"+runningTotal+".00");
+	getCheese(runningTotal,text1); // All three of these variables will be passed on to each function
+};	
+function getCheese(runningTotal,text1) {
+	var cheeseTotal = 0;
+	var cheeseArray = document.getElementsByClassName("cheese");
+	for (var i = 0; i < cheeseArray.length; i++) {
+		if (cheeseArray[i].checked) {
+			var selectedCheese = cheeseArray[i].value;
+			text1 = text1+selectedCheese+"<br>";
+		}
+	}
+	if (selectedCheese === "Extra Cheese") {
+		cheeseTotal = 3;
+};
+	/* Learned I didn't even need to make it this complicated
+
+
+if (selectedCheese === "Cheese") {      
+		cheeseTotal = 0;
+	} else if (selectedCheese === "No cheese") {
+		cheeseTotal = 0;
+	} else if (selectedCheese === "Extra Cheese") {
+		cheeseTotal = 3;*/
+	runningTotal = (runningTotal + cheeseTotal);
+	console.log(selectedCheese+" = $"+cheeseTotal+".00");
+	console.log("cheese text1: "+text1);
+	console.log("subtotal: $"+runningTotal+".00");
+	getCrust(runningTotal,text1); // All three of these variables will be passed on to each function
+};	
+
+function getCrust(runningTotal,text1) {
+	var crustTotal = 0;
+	var crustArray = document.getElementsByClassName("crust");
+	for (var i = 0; i < crustArray.length; i++) {
+		if (crustArray[i].checked) {
+			var selectedCrust = crustArray[i].value;
+			text1 = text1+selectedCrust+"<br>";
+		}
+	}
+	if (selectedCrust === "Cheese Stuffed Crust") {
+		crustTotal = 3;
+};
+	
+	runningTotal = (runningTotal + crustTotal);
+	console.log(selectedCrust+" = $"+crustTotal+".00");
+	console.log("crust text1: "+text1);
+	console.log("subtotal: $"+runningTotal+".00");
+	getMeat(runningTotal,text1); // All three of these variables will be passed on to each function
+};	
+
+
+// With both the meat and cheese functions each item in the array will be
+// 1 dollar but the first is going to be free so we can count the total
+// of items in their array and subtract 1 to get the total item cost
+//
+// Now we can add the item cost to our running total to get the new
+// running total and then pass this new running total to the next function
+// Just keep up this process until we've added all items to the running total
+function getMeat(runningTotal,text1) {
+	var meatTotal = 0;
+	var selectedMeat = [];
+	var meatArray = document.getElementsByClassName("meats");
+	for (var j = 0; j < meatArray.length; j++) {
+		if (meatArray[j].checked) {
+			selectedMeat.push(meatArray[j].value);
+			console.log("selected meat item: ("+meatArray[j].value+")");
+			text1 = text1+meatArray[j].value+"<br>";
+		}
+	}
+	var meatCount = selectedMeat.length;
+	if (meatCount > 1) {
+		meatTotal = (meatCount - 1);
+	} else {
+		meatTotal = 0;
+	}
+	runningTotal = (runningTotal + meatTotal);
+	console.log("total selected meat items: "+meatCount);
+	console.log(meatCount+" meat - 1 free meat = "+"$"+meatTotal+".00");
+	console.log("meat text1: "+text1);
+	console.log("Purchase Total: "+"$"+runningTotal+".00");
+	document.getElementById("showText").innerHTML=text1;
+	document.getElementById("totalPrice").innerHTML = "<h3>Total: <strong>$"+runningTotal+".00"+"</strong></h3>";
+	//runningTotal = (runningTotal + meatTotal); remember kids don't double your code on accident
+/*	for (var j = 0; j < selectedMeat.length; j++) {
+			text1 = text1+selectedMeat[j]+"<br>";
+			
+	}*/
+	getVeggie(runningTotal,text1);
+};
+	//pretty much the same as the meat JS, but I would be lying if I didn't tell you how many times I screwed this up because of typos.
+function getVeggie(runningTotal,text1) {
+	var veggieTotal = 0;
+	var selectedVeggie = [];
+	var veggieArray = document.getElementsByClassName("veggie");
+	for (var j = 0; j < veggieArray.length; j++) {
+		if (veggieArray[j].checked) {
+			selectedVeggie.push(veggieArray[j].value);
+			console.log("selected veggie item: ("+veggieArray[j].value+")");
+			text1 = text1+veggieArray[j].value+"<br>";
+		}
+	}
+	var veggieCount = selectedVeggie.length;
+	if (veggieCount > 1) {
+		veggieTotal = (veggieCount - 1);
+	} else {
+		veggieTotal = 0;
+	}
+	runningTotal = (runningTotal + veggieTotal);
+	console.log("total selected veggie items: "+veggieCount);
+	console.log(veggieCount+" veggie - 1 free veggie = "+"$"+veggieTotal+".00");
+	console.log("veggie text1: "+text1);
+	console.log("Purchase Total: "+"$"+runningTotal+".00");
+	document.getElementById("showText").innerHTML=text1;
+	document.getElementById("showText").innerHTML=text1;
+	document.getElementById("totalPrice").innerHTML = "<br><br><br><h3>Total: <strong>$"+runningTotal+".00"+"</strong></h3>";
+	getSauce(runningTotal, text1)
+};
+function getSauce(runningTotal,text1) {
+	var sauceTotal =0;
+	var sauceArray = document.getElementsByClassName("sauce");
+	for (var i = 0; i < sauceArray.length; i++) {
+		if (sauceArray[i].checked) {
+			var selectedSauce = sauceArray[i].value;
+			text1 = text1+sauceArray+"<br>";
+		}
+	}
+	
+	if (selectedSauce === "marinara") { 
+		sauceTotal = 0;
+}; 
+	
+	runningTotal = (runningTotal + sauceTotal);
+	console.log(selectedSauce+" = $"+sauceTotal+".00");
+	console.log("sauce text1: "+text1);
+	console.log("subtotal: $"+runningTotal+".00");
+	//getMeat(runningTotal,text1); // All three of these variables will be passed on to each function
+};	
+
+
+
+
+
